314bdc3a3bdfd21d1f40b5535e1935476a82bf60
LibreGML <135242529+LibreGML@users.noreply.github.com>
2024-10-07 08:03:48 +0000

    Add files via upload

---
+++ scripts/out.ps1
@@ -0,0 +1,15 @@
+$session = New-Object Microsoft.PowerShell.Commands.WebRequestSession
+$session.UserAgent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36 Edg/129.0.0.0"
+Invoke-WebRequest -UseBasicParsing -Uri "http://10.255.254.13/api/portal/v1/logout" `
+-Method "POST" `
+-WebSession $session `
+-Headers @{
+"Accept"="application/json, text/javascript, */*; q=0.01"
+  "Accept-Encoding"="gzip, deflate"
+  "Accept-Language"="zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
+  "Origin"="http://10.255.254.13"
+  "Referer"="http://10.255.254.13/portal/mobile.html?v=202108261127"
+  "X-Requested-With"="XMLHttpRequest"
+} `
+-ContentType "application/x-www-form-urlencoded; charset=UTF-8" `
+-Body "{`"domain`":`"default`"}"
